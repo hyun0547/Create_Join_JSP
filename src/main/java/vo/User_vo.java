f1ea95d204af130c0002663108d3c64983de5f4f
hyun0547 <hyun0547@gmail.com>
2021-11-05 21:13:52 +0900

    New JSP project

---
+++ src/main/java/vo/User_vo.java
@@ -0,0 +1,13 @@
+package vo;
+
+public class User_vo {
+	private int num;
+	private String id;
+	private String name;
+	private String password;
+	private String gender;
+	private String zipcode;
+	private String address;
+	private String addressDetail;
+	private String addressEtc;
+}
